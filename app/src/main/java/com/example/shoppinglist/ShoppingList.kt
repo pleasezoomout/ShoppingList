ef24c8f2d64e372f2dcf569cb7e10d786af52bb5
pleasezoomout <sebastiansalamanca@Sebastians-MacBook-Air.local>
2024-07-26 23:08:33 -0400

    item list view

--- app/src/main/java/com/example/shoppinglist/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingList.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
@@ -28,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -67,7 +71,9 @@ fun ShoopingListApp(modifier: Modifier = Modifier){
                 .fillMaxSize()
                 .padding(16.dp)
         ) {
-            items(sItems)
+            items(sItems){
+                ShoppingListItem(it, {},{})
+            }
         }
     }
 
@@ -102,7 +108,9 @@ fun ShoopingListApp(modifier: Modifier = Modifier){
             confirmButton = {
 
                 Row(
-                    modifier = Modifier.fillMaxWidth().padding(16.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
                     horizontalArrangement = Arrangement.SpaceBetween,
                 ){
                     Button(
@@ -133,10 +141,31 @@ fun ShoopingListApp(modifier: Modifier = Modifier){
 
 fun items(count: List<ShoopingItem>) {}
 
+@Composable
+fun ShoppingListItem(
+    item: ShoopingItem,
+    onEdit: (ShoopingItem) -> Unit,
+    onDelete: (ShoopingItem) -> Unit) {
+
+    Row(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxWidth()
+        .border(
+            border = BorderStroke(2.dp, Color.Cyan),
+            shape = RoundedCornerShape(20)
+        ),) {
+        
+        Text(text = item.name, modifier = Modifier.padding(16.dp))
+    
+    }
+}
+
+
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ShoppingListItem() {
     ShoppingListTheme {
         ShoopingListApp()
     }
